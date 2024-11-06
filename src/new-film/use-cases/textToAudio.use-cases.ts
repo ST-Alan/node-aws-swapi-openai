f797085c164f21ff40a0d84c45829420573dd0c3
Alan Fermin <61783888+ST-Alan@users.noreply.github.com>
2024-11-06 12:26:14 -0500

    Update textToAudio.use-cases.ts

--- src/new-film/use-cases/textToAudio.use-cases.ts
+++ src/new-film/use-cases/textToAudio.use-cases.ts
@@ -20,20 +20,14 @@ export const texToAudioUseCase = async(openai:OpenAI,{nuevaSagaEscrita, voice}:O
         shimmer:'shimmer',
     }
 
-    //Esto lo que hace es que busque entre las voices que estan disponibles, y si en voices[voice] no existen las o si devuelve null, entonces que use la voz de nova
-    const selectedVoice = voices[voice] ?? 'nova'
-    //El path se importa de node, se colocar el * as para que tome todo el path
-    // Se puede tener una carpeta USERID para saber que usaurio genero cual archivo, o almacenar todo en la base de datos para saber que usuario guardo que cosa.
-    //const folderPath = path.resolve(__dirname, '../../../generated/USERID/audios')
-    //Aca es donde quiero guardar mi audio
-    const folderPath = path.resolve(__dirname, '../../../generated/audios/')
-    //TExto del audio propiamente:
-    const speechFile = path.resolve(`${folderPath}/${new Date().getTime()}.mp3`)
-    //Aca si 2 personas suben algo al mismo tiempo se puede sobreescribir, seria bueno poner el id del usuario o un uui en esta url:
-    //const speechFile = path.resolve(`${folderPath}/${new Date().getTime()}.mp3`)
 
-    //Aca nos aseguramos de que el directorio exista. Se duplica la importacion del path si se coloca fs
-    // Esto hace que si no existe el directorio que vaya y los cree todos. Por la bandera recursive en true
+    const selectedVoice = voices[voice] ?? 'nova'
+
+    // const folderPath = path.resolve(__dirname, '../../../generated/audios/') //Para correr en local
+    const folderPath = '/tmp'; //AWS
+
+    const speechFile = path.resolve(`${folderPath}/${new Date().getTime()}.mp3`)
+
     fs.mkdirSync( folderPath,{recursive:true})
 
 
